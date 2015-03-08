93659735b11a5668f0b0c7f8ca6e5f23b4f8296f
JohnFeng <fengguangwei.007@gmail.com>
2015-03-09 00:51:52 +0800

    Add: Button Event

--- mobile/src/main/java/me/johnfeng/findandroid/MobileMainActivity.java
+++ mobile/src/main/java/me/johnfeng/findandroid/MobileMainActivity.java
@@ -1,39 +1,115 @@
 package me.johnfeng.findandroid;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Toast;
 
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat.WearableExtender;
 
-public class MobileMainActivity extends Activity {
+import com.dd.CircularProgressButton;
+
+public class MobileMainActivity extends Activity implements Handler.Callback {
+
+    private Handler mHandler;
+    private static final int FIND_TIME = 5000;
+    CircularProgressButton findButton;
+    boolean isProgressing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mobile_act_main);
+        mHandler = new Handler(this);
+
+        findButton = (CircularProgressButton) findViewById(R.id.findButton);
+        findButton.setIndeterminateProgressMode(true); // turn on indeterminate progress
+        findButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startFindAndroid();
+            }
+        });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    void startFindAndroid() {
+        if (isProgressing) {
+            Toast.makeText(this, "Please Wait a Sec...", Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        } else {
+            isProgressing = true;
+            setFindButtonState(50);
+
+            Message message = new Message();
+            message.what = 100;
+            mHandler.sendMessageDelayed(message, FIND_TIME);
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    /**
+     * @param state normal state [0]
+     *              progress state [1-99]
+     *              success state [100]
+     *              error state [-1]
+     */
+    void setFindButtonState(int state) {
+        if (findButton == null) {
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        if (state < -1 || state > 100) {
+            return;
+        }
+
+        findButton.setProgress(state);
+    }
+
+    void buildNotification() {
+
+        int notificationId = 001;
+// Build intent for notification content
+        Intent viewIntent = new Intent(this, MobileMainActivity.class);
+        viewIntent.putExtra(EXTRA_EVENT_ID, eventId);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable)
+                        .setContentTitle(eventTitle)
+                        .setContentText(eventLocation)
+                        .setContentIntent(viewPendingIntent);
+
+// Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+// Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
+    }
+
+
+    @Override
+    public boolean handleMessage(final Message msg) {
+        if (msg.what == 0) {
+            setFindButtonState(0);
+            isProgressing = false;
+        } else {
+            // 100 && -1
+            setFindButtonState(msg.what);
+            Message message = new Message();
+            message.what = 0;
+            mHandler.sendMessageDelayed(message, 1500);
+        }
+        return true;
     }
 }
